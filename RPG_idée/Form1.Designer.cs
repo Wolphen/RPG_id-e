a069413b1c0b91b01ed4cb035dbb77e456df6ea6
Wolphen <enzo.grignon@edu.esiee.fr>
2023-09-26 16:21:35 +0200

    Ajoutez des fichiers projet.

---
+++ RPG_idée/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RPG_idée
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.Font = new Font("hades", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(613, 309);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(421, 126);
+            button2.TabIndex = 2;
+            button2.Text = "Start";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            button2.MouseEnter += button2_MouseEnter;
+            button2.MouseLeave += button2_MouseLeave;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Control;
+            button1.BackgroundImage = Properties.Resources.backgorund_tittle;
+            button1.Font = new Font("hades", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Control;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(613, 460);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(421, 124);
+            button1.TabIndex = 3;
+            button1.Text = "Quit";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button2_Click_1;
+            button1.MouseEnter += button1_MouseEnter;
+            button1.MouseLeave += button1_MouseLeave;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Cursor = Cursors.IBeam;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("hades", 140.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(536, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(584, 195);
+            label2.TabIndex = 6;
+            label2.Text = "Ether";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1556, 884);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private Label label2;
+    }
+}
---
+++ RPG_idée/Form1.cs
@@ -0,0 +1,49 @@
+using System.Drawing.Imaging;
+
+namespace RPG_idée
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // label2.Parent = pictureBox1;
+            label2.BackColor = Color.Transparent;
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Black;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.White;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.Black;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.White;
+        }
+    }
+}
