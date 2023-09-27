e4ff954820d1903113988bab1a134c9879ac40dc
Wolphen <enzo.grignon@edu.esiee.fr>
2023-09-27 09:00:09 +0200

    Actualisation

--- RPG_idée/Form1.Designer.cs
+++ RPG_idée/Form1.Designer.cs
@@ -41,10 +41,9 @@
             button2.Font = new Font("hades", 48F, FontStyle.Regular, GraphicsUnit.Point);
             button2.ForeColor = SystemColors.ButtonHighlight;
             button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.Location = new Point(613, 309);
-            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Location = new Point(720, 422);
             button2.Name = "button2";
-            button2.Size = new Size(421, 126);
+            button2.Size = new Size(382, 110);
             button2.TabIndex = 2;
             button2.Text = "Start";
             button2.UseVisualStyleBackColor = true;
@@ -55,14 +54,13 @@
             // button1
             // 
             button1.BackColor = SystemColors.Control;
-            button1.BackgroundImage = Properties.Resources.backgorund_tittle;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
             button1.Font = new Font("hades", 48F, FontStyle.Regular, GraphicsUnit.Point);
             button1.ForeColor = SystemColors.Control;
             button1.Image = (Image)resources.GetObject("button1.Image");
-            button1.Location = new Point(613, 460);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(720, 609);
             button1.Name = "button1";
-            button1.Size = new Size(421, 124);
+            button1.Size = new Size(382, 105);
             button1.TabIndex = 3;
             button1.Text = "Quit";
             button1.UseVisualStyleBackColor = false;
@@ -78,26 +76,25 @@
             label2.FlatStyle = FlatStyle.Flat;
             label2.Font = new Font("hades", 140.25F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(536, 75);
+            label2.Location = new Point(596, 101);
             label2.Name = "label2";
-            label2.Size = new Size(584, 195);
+            label2.Size = new Size(731, 244);
             label2.TabIndex = 6;
             label2.Text = "Ether";
             label2.Click += label2_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1556, 884);
+            ClientSize = new Size(1778, 1102);
             Controls.Add(label2);
             Controls.Add(button1);
             Controls.Add(button2);
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             WindowState = FormWindowState.Maximized;
             Load += Form1_Load;
